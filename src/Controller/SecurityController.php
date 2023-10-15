7497deb4671c63260efdcfd2078f4f98ba5822f5
Marion <mariondoubeck@yahoo.fr>
2023-10-15 19:46:41 +0200

    reset password ok

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,9 +2,18 @@
 
 namespace App\Controller;
 
+use App\Form\ResetPasswordFormType;
+use App\Form\ResetPasswordRequestFormType;
+use App\Repository\UserRepository;
+use App\Service\SendMailService;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Csrf\TokenGenerator\TokenGeneratorInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
@@ -31,5 +40,98 @@ class SecurityController extends AbstractController
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    #[Route(path: '/oubli-mot-de-passe', name: 'forgotten_password')]
+    public function forgotten_password(
+        Request $request,
+        UserRepository $userRepository,
+        TokenGeneratorInterface $tokenGeneratorInterface,
+        EntityManagerInterface $em,
+        SendMailService $mail,
+    ): Response
+    {
+        $form = $this->createForm(ResetPasswordRequestFormType::class);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() === TRUE && $form->isValid() === TRUE){
+            //look for user from username
+            $user = $userRepository->findOneByUsername($form->get('username')->getData());
+            if($user){
+                //generate reset Token
+                $token = $tokenGeneratorInterface->generateToken();
+                $user->setResetToken($token);
+                $em->persist($user);
+                $em->flush();
+
+                //generate reset password link
+                $url = $this->generateUrl('reset_pass', ['token' => $token], UrlGeneratorInterface::ABSOLUTE_URL);
+
+                //Send the mail
+                $mail->send(
+                    'no-reply@freestyle.net',
+                    $user->getEmail(),
+                    'Réinitialisez votre mot de passe Snowboard Freestyle',
+                    'password_reset',
+                    [
+                        'user' => $user,
+                        'url' => $url
+                    ]
+                );
+
+                $this->addFlash('success', 'Email envoyé avec succès');
+                return $this->redirectToRoute('app_login');
+            }
+
+            //else : if $user doesn't exist with this username (===null)
+            $this->addFlash('danger', 'Un problème est survenu');
+            return $this->redirectToRoute('app_login');
+        }
+        
+        return $this->render('security/reset_password_request.html.twig',[
+            'requestPassForm' => $form->createView(),
+        ]);
+    }
+
+    //route for resetpassword link
+    #[Route(path: '/oubli-passe/{token}', name: 'reset_pass')]
+    public function resetPass(
+        string $token,
+        Request $request,
+        UserRepository $userRepository,
+        EntityManagerInterface $entityManager,
+        UserPasswordHasherInterface $passwordHasher
+    ): Response
+    {
+        //check token
+        $user = $userRepository->findOneByResetToken($token);
+        if($user){
+            $form = $this->createForm(ResetPasswordFormType::class);
+            $form->handleRequest($request);
+            if($form->isSubmitted() === TRUE && $form->isValid() === TRUE){
+                //delete token
+                $user->setResetToken(null);
+                // encode the plain password
+                $user->setPassword(
+                    $passwordHasher->hashPassword(
+                        $user,
+                        $form->get('plainPassword')->getData()
+                    )
+                );
+
+                $entityManager->persist($user);
+                $entityManager->flush();
+
+                $this->addFlash('success', 'Mot de passe modifié avec succès');
+                return $this->redirectToRoute('app_login');
+            }
+
+            return $this->render('security/reset_password.html.twig',[
+                'passForm' => $form->createView(),
+            ]);
+        }
+        //else : if $user doesn't exist with this username (===null)
+        $this->addFlash('danger', 'Ce lien n\'est pas valide');
+        return $this->redirectToRoute('app_login');
     }
 }
