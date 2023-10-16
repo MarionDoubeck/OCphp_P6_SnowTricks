2a55fc04f8b3223873ea533d9f092f1c6b8e75dc
Marion <mariondoubeck@yahoo.fr>
2023-10-16 19:28:18 +0200

    debug comment 404 new tricks

--- src/Controller/ErrorController.php
+++ src/Controller/ErrorController.php
@@ -8,7 +8,7 @@ use Symfony\Component\HttpFoundation\Response;
 class ErrorController extends AbstractController
 {
     public function handleError(\Throwable $exception): Response
-    {
+    {   dd($exception);
         if (method_exists($exception, 'getStatusCode') && $exception->getStatusCode() === 404){
             return $this->render('error/error404.html.twig', []);
         } elseif (method_exists($exception, 'getStatusCode')) {
