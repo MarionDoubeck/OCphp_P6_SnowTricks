f35089a83f47623fdc0b9934b0b5f2609d7d91d7
Marion <mariondoubeck@yahoo.fr>
2024-01-21 10:46:54 +0100

    erase unused

--- public/assets/js/mainNonMin.js
+++ public/assets/js/mainNonMin.js
@@ -183,29 +183,6 @@
 
   });
 
-  /**
-   * Initiate portfolio lightbox 
-   */
-  const portfolioLightbox = GLightbox({
-    selector: ".portfolio-lightbox"
-  });
-
-  /**
-   * Portfolio details slider
-   */
-  new Swiper(".portfolio-details-slider", {
-    speed: 400,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      type: 'bullets',
-      clickable: true
-    }
-  });
 
   /**
    * Skills animation
@@ -223,34 +200,6 @@
       }
     })
   }
-
-  /**
-   * Testimonials slider
-   */
-  new Swiper(".testimonials-slider", {
-    speed: 600,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    slidesPerView: 'auto',
-    pagination: {
-      el: ".swiper-pagination",
-      type: 'bullets',
-      clickable: true
-    },
-    breakpoints: {
-      320: {
-        slidesPerView: 1,
-        spaceBetween: 40
-      },
-
-      1200: {
-        slidesPerView: 3,
-      }
-    }
-  });
 
   /**
    * Initiate Pure Counter 
